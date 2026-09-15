f51bc5c8b1c0ddf80ac0df9e67b9ff878afa1d0b
Raheel Fida <nabeelfida185@gmail.com>
2026-09-15 21:58:40 +0500

    update main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 
+# This initializes the Pygame systems we're going to use.
 pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
@@ -7,9 +8,11 @@ pygame.display.set_caption("My Ludo Game")
 
 running = True
 
+# is the game loop. 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            # If the user clicks the close button
             running = False
 
 pygame.quit()
