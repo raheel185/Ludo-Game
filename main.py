fa7ad6791a2bd5ebea07172b8a8be65cf6cbb44f
Raheel Fida <nabeelfida185@gmail.com>
2026-09-16 06:16:46 +0500

    Update

--- main.py
+++ main.py
@@ -158,6 +158,13 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+
+            if event.key == pygame.K_RIGHT:
+                token_position += 1
+
+            if event.key == pygame.K_LEFT:
+                token_position -= 1
 
     screen.fill("white")
 
